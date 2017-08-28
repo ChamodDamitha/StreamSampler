a0f082fba2f06958c11f6c92b1d9f4e049049023
chamod <chamod.14@cse.mrt.ac.lk>
2017-08-28 17:55:39 +0530

    Stream sampling on average done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,51 @@
-/**
- * Created by chamod on 8/28/17.
- */
+/*
+* Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+* WSO2 Inc. licenses this file to you under the Apache License,
+* Version 2.0 (the "License"); you may not use this file except
+* in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied. See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
 public class Main {
     public static void main(String[] args) {
+
+        final int noOfEvents = 1000;
+        final double accuracy = 0.99;
+        int real_sum = 0;
+        int sample_sum = 0;
+
+        StreamSampler<Integer> streamSampler = new StreamSampler(accuracy);
+
+        for (int i = 0; i < noOfEvents; i++) {
+            real_sum += i;
+            streamSampler.add(i);
+        }
+
+        for (Integer j : streamSampler.getEvents()) {
+            sample_sum += j;
+        }
+
+
+        System.out.println("Accuracy : " + streamSampler.getAccuracy());
+        System.out.println("Stream Sampler Size : " + streamSampler.getEvents().size());
+        System.out.println("Real Average : " + (real_sum * 1.0 / noOfEvents));
+
+        double approximate_answer = (sample_sum * 1.0 / streamSampler.getEvents().size());
+
+        System.out.println("Sample Average : " + approximate_answer);
+
+        System.out.println("Confidence Interval : [" + (approximate_answer - approximate_answer * (1-accuracy)) + ", " +
+                (approximate_answer + approximate_answer * (1-accuracy)));
+
 
     }
 }
--- src/main/java/StreamSampler.java
+++ src/main/java/StreamSampler.java
@@ -1,6 +1,74 @@
-/**
- * Created by chamod on 8/28/17.
- */
-public class StreamSampler {
+/*
+* Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+* WSO2 Inc. licenses this file to you under the Apache License,
+* Version 2.0 (the "License"); you may not use this file except
+* in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied. See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
 
+import java.util.ArrayList;
+
+public class StreamSampler<T> {
+    private double accuracy;
+    private int totalNoOfHashValues;
+    private int acceptedNoOfHashValues;
+
+    private ArrayList<T> events;
+
+    public StreamSampler(double accuracy) {
+        if (accuracy >= 1 || accuracy <= 0) {
+            throw new IllegalArgumentException("accuracy must be in the range of (0,1)");
+        }
+        this.accuracy = accuracy;
+
+        String accuracyStr = accuracy + "";
+
+        int decimalLength = (accuracyStr.split("\\.")[1]).length();
+        int multiFactor = 1;
+        for (int i = 0; i < decimalLength; i++) {
+            multiFactor *= 10;
+        }
+        acceptedNoOfHashValues = multiFactor - (int) Math.ceil(accuracy * multiFactor);
+        totalNoOfHashValues = multiFactor;
+
+        events = new ArrayList<T>();
+    }
+
+    public boolean add(T t) {
+        if (isAddable(t)) {
+            events.add(t);
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isAddable(T t) {
+        if (getHashValue(t) <= acceptedNoOfHashValues) {
+            return true;
+        }
+        return false;
+    }
+
+    private int getHashValue(T t) {
+        int hash = MurmurHash.hash(t);
+        return hash % totalNoOfHashValues;
+    }
+
+    public double getAccuracy() {
+        return accuracy;
+    }
+
+    public ArrayList<T> getEvents() {
+        return events;
+    }
 }
